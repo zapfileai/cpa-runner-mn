5ad0a43d842b5350feabe0b83d91796e99ac8f81
Sreehari Annam <sannam@wescodist.com>
2026-06-30 07:19:13 -0400

    feat: load first 80 cities from cities.csv

--- cities.js
+++ cities.js
@@ -1,5 +1,20 @@
-﻿const minnesotaCities = [
-  // paste first 80 cities from cities.csv here
+const minnesotaCities = [
+  "Ada", "Adams", "Afton", "Aitkin", "Albany",
+  "Albert Lea", "Albertville", "Alden", "Alexandria", "Amboy",
+  "Andover", "Annandale", "Anoka", "Appleton", "Arlington",
+  "Ashby", "Aurora", "Austin", "Avon", "Bagley",
+  "Balaton", "Barnesville", "Barnum", "Battle Lake", "Baxter",
+  "Bayport", "Becker", "Belle Plaine", "Bemidji", "Benson",
+  "Bertha", "Bethel", "Big Lake", "Bird Island", "Biwabik",
+  "Blooming Prairie", "Blue Earth", "Bovey", "Boyd", "Braham",
+  "Brainerd", "Brandon", "Breckenridge", "Brewster", "Britt",
+  "Brooten", "Buffalo", "Burnsville", "Byron", "Caledonia",
+  "Cambridge", "Canby", "Cannon Falls", "Carlton", "Carver",
+  "Cedar", "Center City", "Champlin", "Chanhassen", "Chaska",
+  "Chatfield", "Chisago City", "Chisholm", "Circle Pines", "Clara City",
+  "Claremont", "Clarkfield", "Clarks Grove", "Clear Lake", "Clearwater",
+  "Cleveland", "Climax", "Cloquet", "Cohasset", "Cokato",
+  "Cold Spring", "Cologne", "Cook", "Cottage Grove", "Cottonwood",
 ];
 
 const BATCH_SIZE = 5;
